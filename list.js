f5d07b43659f15eb2a2a3a90d71dd1f17defc6c7
dluman <dluman@allegheny.edu>
2022-06-16 11:25:55 -0400

    Adding list toy example

--- list.js
+++ list.js
@@ -1,12 +1,14 @@
 const Docker = require("dockerode");
 const ishmael = new Docker({socketPath: '/var/run/docker.sock'});
 
-//ishmael.listContainers({all:true}, (err, containers) => {
-//  containers.forEach((entry) => {
-//    let container = ishmael.getContainer(entry.Id);
-//    container.stop();
-//  });
-//});
+ishmael.listContainers({all:true}, (err, containers) => {
+  containers.forEach((entry) => {
+    let container = ishmael.getContainer(entry.Id);
+    container.stop((err, data) => {
+      container.remove();
+    });
+  });
+});
 
 let now = Math.floor(new Date().getTime() / 1000)
 ishmael.pruneContainers({until: now})
